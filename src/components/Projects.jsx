5ac9519adb9b0947a1e61da305f0fd9386a314d7
Paul Morrow <pmorow93@gmail.com>
2023-02-15 08:17:29 -0600

    added a projects section and a little animation to contact section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div>
-        <p>
-            Projects will go here!
+    <div className='bg-teal-600 flex-col justify-center'>
+        <p className='text-4xl text-center text-white'>
+            Projects
         </p>
+        <div className='flex justify-evenly mt-5 pb-5 text-cyan-200 text-2xl'>
+            <div>Project 1</div>
+            <div>Project 2</div>
+            <div>Project 3</div>
+            <div>Project 4</div>
+            <div>Project 5</div>
+        </div>
     </div>
   )
 }
