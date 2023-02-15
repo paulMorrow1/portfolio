74a9b90f7f536d14531574981291451061e15863
Paul Morrow <pmorow93@gmail.com>
2023-02-15 08:19:30 -0600

    made a small change to animation in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,12 @@ import React from 'react';
 const Footer = () => {
   return (
     <div className='bg-teal-700 h-full w-full'>
-        <h2 className='text-4xl text-white flex justify-center pt-8 pb-3 animate-bounce'>Want to reach out? Contact me here!</h2>
+        <h2 className='text-4xl text-white flex justify-center pt-8 pb-3 '>
+            Want to reach out?
+            <span className='animate-bounce pl-3'>
+                Contact me here!
+            </span>
+        </h2>
         <div className='flex justify-evenly pt-3 pb-[50px]'>
             <p className='text-2xl text-teal-300 text-center'>
                 email: pmorow93@gmail.com
