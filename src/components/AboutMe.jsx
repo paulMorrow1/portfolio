5026ce7ce3aef3a24af178ab62c12a19f1b2d509
Paul Morrow <pmorow93@gmail.com>
2023-02-22 08:05:28 -0600

    updated styling in Projects section, brainstorming how to create individual project cards

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -6,7 +6,7 @@ const AboutMe = () => {
             <div className='flex'>
             <img className="w-[500px] h-75% m-5 border-4 border-solid border-cyan-800"src={profilepic} alt='profile'/>
             <div>
-                <h2 className='pt-5 text-4xl underline'>About Me</h2>
+                <h2 className='pt-5 text-4xl underline text-blue-900'>About Me</h2>
                 <p className="bg-cyan-100 text-base p-10 m-4 border-4 border-solid border-cyan-800">
                     About me section. A paragraph or two about myself and my journey to becomming a Developer. hello there dlkjsdhg;j ldsjkhbvlksjh askjh blakudjhva sdkgjlahsdfg;kljsfng zsal;kjnxc ;adlifgha ;fbkujhacbv ;asdfujh a. ;sdljkhgskuvhsdfkjv dsfkuihn fdscxkluhsd flvk;ujsdhf vlk;adjfhn ;adigha df;lguhdr gkjdfuhg a;ldfugha sdfklg;uh g. dsjkhbvlksjh askjh blakudjhva sdkgjlahsdfg;kljsfng zsal;kjnxc ;adlifgha ;fbkujhacbv ;asdfujh a. ; </p>
             </div>                
---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ProjectCard = () => {
+  const [card, setCard] = {};
+  return (
+    <div className='border-solid border-4 border-cyan-900'>Project</div>
+  )
+}
+
+export default ProjectCard;
