63ba517a8d436f658720744fcc0e6d5c3e99c0c9
Paul Morrow <paulmorrow@Pauls-MacBook-Pro.local>
2024-02-20 22:50:07 -0600

    added project descriptions and added styling to footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,10 +4,10 @@ import "./index.css";
 const Footer = () => {
   return (
     <div className="footer">
-      <h2 className="footer-message">
-        Want to reach out?
-        <span className="footer-message-span"> Contact me here!</span>
-      </h2>
+      <div className="footer-message">
+        <h2>Want to reach out?</h2>
+        <h2 className="footer-message-bounce">Contact me here!</h2>
+      </div>
       <div className="contact-info">
         <p>email: pmorow93@gmail.com</p>
         <p>Mobile: (402)657-7972</p>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -20,41 +20,58 @@ const Projects = () => {
         })}
       </div>
       <div className="iframes">
-        <iframe
-          title="calculator"
-          src="projects/calculator/index.html"
-          width={460}
-          height={660}
-          className="iframe-calculator"
-        />
-        <iframe
-          title="rock paper scissors"
-          src="projects/rock_paper_scissors/index.html"
-          width={700}
-          height={700}
-          className="iframe-rock-paper-scissors"
-        />
-        <iframe
-          title="Quiz"
-          src="projects/quiz/index.html"
-          width={700}
-          height={700}
-          className="iframe-quiz"
-        />
-        <iframe
-          title="movie catalog"
-          src="https://movie-catalog-eight-tau.vercel.app/"
-          width={700}
-          height={700}
-          className="iframe-movie-catalog"
-        />
-        <iframe
-          title="Star Wars"
-          src="https://star-wars-three-alpha.vercel.app/"
-          width={700}
-          height={700}
-          className="iframe-star-wars"
-        />
+        <div className="calculator-container">
+          <iframe
+            title="calculator"
+            src="projects/calculator/index.html"
+            width={460}
+            height={660}
+            className="iframe-calculator"
+          />
+          <p className="calculator-description">{projects[0].description}</p>
+        </div>
+        <div className="rock-paper-scissors-container">
+          <iframe
+            title="rock paper scissors"
+            src="projects/rock_paper_scissors/index.html"
+            width={700}
+            height={400}
+            className="iframe-rock-paper-scissors"
+          />
+          <p className="rock-paper-scissors-description">
+            {projects[1].description}
+          </p>
+        </div>
+        <div className="quiz-container">
+          <iframe
+            title="Quiz"
+            src="projects/quiz/index.html"
+            width={700}
+            height={500}
+            className="iframe-quiz"
+          />
+          <p className="quiz-description">{projects[2].description}</p>
+        </div>
+        <div className="movie-catalog-container">
+          <iframe
+            title="movie catalog"
+            src="https://movie-catalog-eight-tau.vercel.app/"
+            width={700}
+            height={700}
+            className="iframe-movie-catalog"
+          />
+          <p className="movie-catalog-description">{projects[3].description}</p>
+        </div>
+        <div className="star-wars-container">
+          <iframe
+            title="Star Wars"
+            src="https://star-wars-three-alpha.vercel.app/"
+            width={700}
+            height={700}
+            className="iframe-star-wars"
+          />
+          <p className="star-wars-description">{projects[4].description}</p>
+        </div>
       </div>
     </div>
   );
